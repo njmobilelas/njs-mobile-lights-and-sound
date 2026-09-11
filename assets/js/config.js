6bcd6a033f7515b15552051f76148f941b33509b
njmobilelas <njmobilelas@gmail.com>
2026-09-11 11:27:07 +0800

    Add files via upload

---
+++ assets/js/config.js
@@ -0,0 +1,6 @@
+window.APP_CONFIG = {
+  APP_ID: 'NJS_MOBILE_LIGHTS_SOUND',
+  BACKUP_VERSION: '1.0.0',
+  SUPABASE_URL: 'https://oncqqkffkyzgzgunardh.supabase.co/',
+  SUPABASE_ANON_KEY: 'sb_publishable_PXiO9EfKIoVksdOyEk1kJw_zD4dglCa'
+};
